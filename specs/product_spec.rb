77b53d1be22f8859456a6f99cf772813bec8b0e6
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-03-14 13:56:38 -0700

    One spec for creation of instance of product + accessing name

--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -1,1 +1,17 @@
 require_relative './spec_helper'
+
+describe FarMar::Product do
+
+  TEST_INFO = {
+    vid: 1,
+    name: "name",
+    pid: 3
+  }
+  describe "Product#new" do
+    it "can create new instances of Product with correct name" do
+      item = FarMar::Market.new(TEST_INFO)
+      item.name.must_equal "name"
+    end
+  end
+
+end
