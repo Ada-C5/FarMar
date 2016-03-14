75d67bdfe5bc0a3734bd53b9c2befb0ff71f1918
Sarah Nahmias <nahmisa@gmail.com>
2016-03-14 14:53:24 -0700

    Added self.all and self.find to Vendors class

--- lib/farmar_vendor.rb
+++ lib/farmar_vendor.rb
@@ -1,5 +1,34 @@
 module FarMar
   class Vendor
-    "I exist!"
+
+    attr_reader :id
+
+    def initialize(vendor_information)
+      @id                  = vendor_information[:id]
+      @name                = vendor_information[:name]
+      @number_of_employees = vendor_information[:street_address]
+      @market_id           = vendor_information[:market_id]
+
+    end
+
+    def self.find(data_file = './support/vendors.csv', id) # returns an instance of Sale where the value of the id field in the CSV matches the passed parameter. Will this be different?  There can be multiple with same ID potentially?
+      vendors = self.all(data_file)
+      vendors.each do |vendor|
+        return vendor if vendor.id == id
+      end
+    end
+
+    def self.all(data_file = './support/vendors.csv') #returns a collection of FarMar::"DataClass" instances, representing all of the Markets described in the CSV.
+
+      data_class_instances = [] #start as an empty array. We will fill with instances from our data file.
+
+      data_class_data = CSV.read(data_file)
+      data_class_data.each do |row|
+        class_instance = self.new( id: row[0].to_i, name: row[1], number_of_employees: row[2].to_i, market_id: row[3].to_i ) #hash used is different based on type of DataClass
+        data_class_instances << class_instance #put it into our collection of instances!
+      end
+      return data_class_instances
+    end
+
   end
 end
--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -4,4 +4,19 @@ describe FarMar::Vendor do
   it "is an object we have access to" do
     FarMar::Vendor.wont_be_nil
   end
+
+  describe "FarMar::Vendor#all" do
+    it "returns an array of instances" do
+    FarMar::Vendor.all.must_be_kind_of(Array)
+    end
+  end
+
+  describe "FarMar::Vendor#find" do
+    it "can find a product instance based on ID" do
+    #we have Vendor IDs 1-2690, and will test at random!
+    id = (1..2690).to_a.sample
+    FarMar::Vendor.find(id).must_be_instance_of(FarMar::Vendor)
+    end
+  end
+
 end
