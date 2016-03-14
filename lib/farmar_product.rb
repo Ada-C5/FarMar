3422f523d74404dfee769232cf9dc5170a62496b
Rowan Cota <cota.rg@gmail.com>
2016-03-14 15:06:06 -0700

    copied all method from markets and edited to work with product data.

--- lib/farmar_product.rb
+++ lib/farmar_product.rb
@@ -1,20 +1,32 @@
 class FarMar::Product
 
-	def initialize(id)
-		@id = id
+    attr_reader :id, :name, :vendor_id
+
+	def initialize(product_hash)
+        # ID - (Fixnum) uniquely identifies the product
+        # Name - (String) the name of the product (not guaranteed unique)
+        # vendor_id - (Fixnum) a reference to which vendor sells this product
+
+        @id = product_hash[:id]
+        @name = product_hash[:name]
+        @vendor_id = product_hash[:vendor_id]
 	end
 
-	def self.all(path_to_csv)
+    def self.all(path_to_csv)
     id = nil
 
-    product_list = []
+    market_list = []
     
     # this iterates through the CSV and assigns values to variables to be used in the account initialization.
     CSV.foreach(path_to_csv) do |row|
         id = row[0]
-        product_list << self.new(id)
+        name = row[1]
+        vendor_id = row[2]
+
+        market_hash = {id: id, name: name, vendor_id: vendor_id}
+        market_list << self.new(market_hash)
     end
-    product_list
-	end
+    market_list
+    end
 
 end
