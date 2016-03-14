f01739adcffa3208904d888ac5f4dd878d6afd79
Jade Vance <jadenicolevance@gmail.com>
2016-03-14 16:41:31 -0700

    Added functionality to sale and sale spec.

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,6 +1,35 @@
-module FarMar
-	class Sale
+class FarMar::Sale
 
+	PENNY_CONVERTER = 100
 	
+	def initialize(csv_data)
+		@csv_data 				= 	csv_data
+		@sale_id 					= 	@csv_data[:sale_id]
+		@amount 					=		@csv_data[:amount] / PENNY_CONVERTER
+		@purchase_time 		= 	@csv_data[:purchase_time]
+		@vendor_id 				=		@csv_data[:vendor_id]
+		@product_id 			= 	@csv_data[:product_id]
+
+	end 
+
+	def self.all
+		sales = []
+
+		CSV.open('./support/markets.csv', 'r') do |csv|
+		  csv.read.each do |line|
+		   sales << self.new(
+		   	sale_id: 				line[0].to_i,
+		   	amount: 				line[1].to_i,
+		   	purchase_time: 	line[2].to_s,
+		   	vendor_id:  		line[3].to_i,
+		   	product_id: 		line[4].to_i
+		   )
+		 end
+		end
+		return sales 
+	end
+
+	def self.find(sale_id)
+		return sale_id
 	end
 end
--- specs/sale_spec.rb
+++ specs/sale_spec.rb
@@ -1,10 +1,20 @@
 require_relative './spec_helper'
-# require_relative '../lib/sale'
 
-describe FarMar::Sale do 
-
+describe FarMar::Sale do
 	it "is an object we have access to" do 
 		FarMar::Sale.wont_be_nil 
 	end
 
+	it "Sale.all is a class method" do 
+		FarMar::Sale.all.wont_be_nil
+	end
+
+	it "Sale.all returns an array" do 
+		assert_equal Array, FarMar::Sale.all.class
+	end
+
+	it "Sale.find(id) returns something" do 
+		market = "Tamale Time"
+		assert_equal market, FarMar::Sale.find(market)
+	end
 end
