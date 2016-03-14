80f6f306b28eccb9291b7a55484c83f7fb3bd5e3
Rowan Cota <cota.rg@gmail.com>
2016-03-14 14:06:58 -0700

    worked it out so that #all currently passes its only test.

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -1,7 +1,22 @@
 class FarMar::Market
 
-	def initialize(id)
-		@id = id
+	def initialize(market_hash)
+	# ID - (Fixnum) a unique identifier for that market
+	# Name - (String) the name of the market (not guaranteed unique)
+	# Address - (String) street address of the market
+	# City - (String) city in which the market is located
+	# County - (String) county in which the market is located
+	# State - (String) state in which the market is located
+	# Zip - (String) zipcode in which the market is located
+	
+	@id = market_hash[:id]
+	@name = market_hash[:name]
+	@address = market_hash[:address]
+	@city = market_hash[:city]
+	@county = market_hash[:county]
+	@state = market_hash[:state]
+	@zip_code = market_hash[:zip_code]
+
 	end
 
 	def self.all(path_to_csv)
@@ -12,7 +27,8 @@ class FarMar::Market
     # this iterates through the CSV and assigns values to variables to be used in the account initialization.
     CSV.foreach(path_to_csv) do |row|
         id = row[0]
-        market_list << self.new(id)
+        market_hash = {id: id}
+        market_list << self.new(market_hash)
     end
     market_list
 	end
