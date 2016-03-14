9b51a7e83f6444c3b5b16226e01669f9d72ea228
Jade Vance <jadenicolevance@gmail.com>
2016-03-14 16:14:56 -0700

    added product and product_spec functionality

--- lib/product.rb
+++ lib/product.rb
@@ -1,5 +1,28 @@
-module FarMar
-	class Product
+class FarMar::Product
+	def initialize(csv_data)
+		@csv_data 			= 	csv_data
+		@product_id 		= 	@csv_data[:product_id]
+		@product_name 	=		@csv_data[:product_name]
+		@vendor_id 			= 	@csv_data[:vendor_id]
+	end 
 
+	def self.all
+		products = []
+
+		CSV.open('./support/products.csv', 'r') do |csv|
+		  csv.read.each do |line|
+		   products << self.new(
+		   	product_id: 		line[0].to_i,
+		   	product_name: 	line[1].to_s,
+		   	vendor_id: 			line[2].to_i,
+		   )
+		 end
+		end
+		return products 
 	end
+
+	def self.find(product_id)
+		return product_id
+	end
+
 end
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -1,10 +1,22 @@
 require_relative './spec_helper'
-# require_relative '../lib/product'
 
 describe FarMar::Product do 
 
 	it "is an object we have access to" do 
 		FarMar::Product.wont_be_nil 
+	end
+
+	it "Product.all is a class method" do 
+		FarMar::Product.all.wont_be_nil
+	end
+
+	it "Product.all returns an array" do 
+		assert_equal Array, FarMar::Product.all.class
+	end
+
+	it "Product.find(id) returns something" do 
+		market = "Tamale Time"
+		assert_equal market, FarMar::Product.find(market)
 	end
 
 end
