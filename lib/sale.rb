637f8379c26a4fcfb26c49c005c71845962231b8
Jessica <jessica.weeber@gmail.com>
2016-03-14 14:17:18 -0700

    added self.find and self.all to sale class. added some tests for this class, all passing.

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,3 +1,39 @@
 class FarMar::Sale
+  attr_reader :id, :amount, :purchase_time, :vendor_id, :product_id
 
+  SALE_FILE = "support/sales.csv"
+
+  def initialize(sale_hash)
+    @id = sale_hash[:id]
+    @amount = sale_hash[:amount]
+    @purchase_time = sale_hash[:purchase_time]
+    @vendor_id = sale_hash[:vendor_id]
+    @product_id = sale_hash[:product_id]
+  end
+
+  def self.all
+    sales = []
+    CSV.foreach(SALE_FILE) do |row|
+      sales << FarMar::Sale.new(id: row[0].to_i, amount: row[1].to_i, purchase_time: row[2],
+        vendor_id: row[3].to_i, product_id: row[4].to_i)
+    end
+    return sales
+
+  end
+
+  def self.find(id)
+    sales = self.all
+    found_id = nil
+    sales.each do |sale|
+      if sale.id == id
+        found_id = sale
+      end
+    end
+    if found_id == nil
+      return "ID not found!"
+    else
+      return found_id
+    end
+  end
+  
 end
--- specs/sale_spec.rb
+++ specs/sale_spec.rb
@@ -1,1 +1,24 @@
 require_relative './spec_helper'
+
+describe FarMar::Sale do
+  it "is an object that is an instance of a Class" do
+    FarMar::Sale.must_be_instance_of Class
+  end
+
+  describe "FarMar::Sale#self.all" do
+    it "should return an incredibly large array" do
+      FarMar::Sale.all.must_be_instance_of Array
+    end
+  end
+
+  describe "FarMar::Sale#self.find" do
+    it "should return 8924 when id 15 passed in" do
+      FarMar::Sale.find(15).amount.must_equal 8924
+    end
+
+    it "should return 'ID not found!' when self.find called by id 13000" do
+      FarMar::Sale.find(13000).must_equal "ID not found!"
+    end
+
+  end
+end
