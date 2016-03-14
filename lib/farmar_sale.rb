1f09a4f30407e3ad8fb0c96cd284b93a93488a7a
Rowan Cota <cota.rg@gmail.com>
2016-03-14 15:33:37 -0700

    changing to 2 spaces.

--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -11,8 +11,8 @@ class FarMar::Sale
     
     # this iterates through the CSV and assigns values to variables to be used in the account initialization.
     CSV.foreach(path_to_csv) do |row|
-        id = row[0]
-        sale_list << self.new(id)
+      id = row[0]
+      sale_list << self.new(id)
     end
     sale_list
 	end
