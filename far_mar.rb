9aa0ee3f492ecd96712f5b1967ed9baac3912018
Rowan Cota <cota.rg@gmail.com>
2016-03-14 12:50:18 -0700

    I'm being lazy and committing a bunch at once to get to baseline.

--- far_mar.rb
+++ far_mar.rb
@@ -5,5 +5,8 @@ require 'csv'
 module FarMar; end
 
 # all of our data classes that live in the module
-require 'lib/farmar_market'
-# ...require all needed classes
+# (require all needed classes)
+require_relative 'lib/farmar_market'
+require_relative 'lib/farmar_product'
+require_relative 'lib/farmar_sale'
+require_relative 'lib/farmar_vendor'
---
+++ specs/market_spec.rb
@@ -0,0 +1,10 @@
+require_relative './spec_helper'
+require_relative '../far_mar'
+
+describe FarMar::Market do 
+
+	it "exists" do
+		FarMar::Market.wont_be_nil
+	end
+
+end
