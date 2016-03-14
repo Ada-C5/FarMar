db41a655e0cb8998add06ee5f14436bb72a31486
Jillian Boshart <Jilliliz@gmail.com>
2016-03-14 11:28:51 -0700

    Fixed the require thing.

--- lib/far_mar.rb
+++ lib/far_mar.rb
@@ -1,10 +1,10 @@
-require csv
+require 'csv'
 
 module FarMar
 
-require lib/farmar_market.rb
-require lib/farmar_product.rb
-require lib/farmar_sale.rb
-require lib/farmar_vendor.rb
+require 'lib/farmar_market.rb'
+require 'lib/farmar_product.rb'
+require 'lib/farmar_sale.rb'
+require 'lib/farmar_vendor.rb'
 
 end
