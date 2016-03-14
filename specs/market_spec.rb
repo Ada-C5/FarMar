5741313362b414c2f33f0589c8935a8dbac00d49
Rowan Cota <cota.rg@gmail.com>
2016-03-14 14:56:39 -0700

    wrote tests for the find method, includes 'magic data' for now.

--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -20,11 +20,11 @@ describe FarMar::Market do
 	end
 
 	it "has a find method" do
-		FarMar::Market.find.wont_be_nil
+		FarMar::Market.find('./support/markets.csv').wont_be_nil
 	end
 
 	it "returns the correct market when self#find is called" do
-		FarMar::Market.find(12).name.must_equal("Coxsackie Farmers Market")
+		FarMar::Market.find(12).name.must_equal("Coxsackie Farmers' Market")
 	end
 
 end
