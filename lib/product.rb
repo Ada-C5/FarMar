e8c79b4a1bff31b82bf22c716e7869a746fae99a
Sarah <sarahkane682@yahoo.com>
2016-03-14 15:39:28 -0700

    massing tests for self.find and self.all methods for product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,4 +1,32 @@
-#require_relative './far_mar'
+#require_relative '../far_mar'
+require 'CSV'
 class FarMar::Product
   include FarMar
+
+  def initialize(id=nil,name=nil,vendor_id=nil)
+    @id = id
+    @name = name.to_s
+    @vendor_id = vendor_id.to_i
+  end
+
+  def self.all
+      products = []
+      CSV.read("./support/products.csv").each do |line| #remember to take off dot when using rake
+      products << self.new(line[0],line[1],line[2])
+    end
+    return products #just as a reminder out side of loop
+  end
+
+  def self.find(id)
+    CSV.read("./support/products.csv").each do |line| #remember to take off dot when using rake
+        if line[0] == id.to_s
+          return self.new(line[0],line[1],line[2])
+        end
+      end
+  end
+
+
+
+
+
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,8 +1,27 @@
 require_relative './spec_helper'
 
 describe FarMar::Product do
+  before do
+    @product = FarMar::Product.new
+  end
 
   it "is an object we have access to" do
-    FarMar::Product.wont_be_nil
+    @product.wont_be_nil
   end
+end
+
+describe FarMar::Product do
+  before do
+  @product = FarMar::Product
+  end
+  it "will return an array of product instances" do
+    @product.all.class.must_equal Array
+  end
+
+  it "will return a product with id 5" do
+    @product.find(5).must_be_instance_of FarMar::Product
+  end
+
+
+
 end
