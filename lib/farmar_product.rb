eb7950b72550c0a6fd4ae6ea23879b0a48172470
Rowan Cota <cota.rg@gmail.com>
2016-03-14 15:14:12 -0700

    product now passes the five tests for existence/all/find.

--- lib/farmar_product.rb
+++ lib/farmar_product.rb
@@ -1,32 +1,39 @@
 class FarMar::Product
 
-    attr_reader :id, :name, :vendor_id
+  attr_reader :id, :name, :vendor_id
 
 	def initialize(product_hash)
-        # ID - (Fixnum) uniquely identifies the product
-        # Name - (String) the name of the product (not guaranteed unique)
-        # vendor_id - (Fixnum) a reference to which vendor sells this product
+    # ID - (Fixnum) uniquely identifies the product
+    # Name - (String) the name of the product (not guaranteed unique)
+    # vendor_id - (Fixnum) a reference to which vendor sells this product
 
-        @id = product_hash[:id]
-        @name = product_hash[:name]
-        @vendor_id = product_hash[:vendor_id]
+    @id = product_hash[:id]
+    @name = product_hash[:name]
+    @vendor_id = product_hash[:vendor_id]
 	end
 
-    def self.all(path_to_csv)
-    id = nil
-
-    market_list = []
+  def self.all(path_to_csv)
+    product_list = []
     
     # this iterates through the CSV and assigns values to variables to be used in the account initialization.
     CSV.foreach(path_to_csv) do |row|
-        id = row[0]
-        name = row[1]
-        vendor_id = row[2]
+      id = row[0]
+      name = row[1]
+      vendor_id = row[2]
 
-        market_hash = {id: id, name: name, vendor_id: vendor_id}
-        market_list << self.new(market_hash)
+      product_hash = {id: id, name: name, vendor_id: vendor_id}
+      product_list << self.new(product_hash)
     end
-    market_list
-    end
+    product_list
+  end
 
+  def self.find(id)
+    products_to_search = FarMar::Product.all("./support/products.csv")
+
+    products_to_search.each do |product|
+        if product.id == id.to_s
+            return product
+        end
+    end
+  end
 end
