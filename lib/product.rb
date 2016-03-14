5128cc08b1ca84ee61f106069c68094b79307211
Yordanos <yordanosd@gmail.com>
2016-03-14 15:51:47 -0700

    Added the self.all method to the products class and passed the test for it.

--- lib/product.rb
+++ lib/product.rb
@@ -1,4 +1,24 @@
 #require_relative '../far_mar'
 
 class FarMar::Product
+  attr_reader
+
+  def initialize(product_info)
+    @id              = product_info [ :id ]              # (Fixnum) uniquely identifies the vendor
+    @name            = product_info [ :name ]            # (String) the name of the vendor (not guaranteed unique)
+    @total_employees = product_info [ :total_employees ] # (Fixnum) How many employees the vendor has at the market
+    @market_id       = product_info [ :market_id ]       # (Fixnum) a reference to which market the vendor attends
+  end
+
+  def self.all
+    all_products = []
+
+    products_from_csv = CSV.read("./support/products.csv") # creates an array with each line as element
+
+    products_from_csv.each do |line|
+      all_products << self.new(id: line[0], name: line[1], total_employees: line[2], market_id: line[3])
+    end
+    return all_products
+  end
+
 end
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -5,3 +5,9 @@ describe FarMar::Product do
     FarMar::Product.wont_be_nil
   end
 end
+
+describe "FarMar::Product#all" do
+  it "should return an array of instances from products.csv file" do
+    FarMar::Product.all.class.must_equal Array
+  end
+end
