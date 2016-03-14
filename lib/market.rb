4bba7c9f0da3d5d91622aa4ca46286f4f1b69093
Jessica <jessica.weeber@gmail.com>
2016-03-14 13:44:32 -0700

    created self.all and self.find methods in market.rb file, plus two passing tests.

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,48 @@
+require 'csv'
+
 class FarMar::Market
 
-end 
+  attr_reader :id, :name, :address, :city, :county, :state, :zip
+
+  MARKET_FILE = "support/markets.csv"
+
+  def initialize(market_hash)
+    @id = market_hash[:id]
+    @name = market_hash[:name]
+    @address = market_hash[:address]
+    @city = market_hash[:city]
+    @county = market_hash[:county]
+    @state = market_hash[:state]
+    @zip = market_hash[:zip]
+  end
+
+  def self.all
+    markets = []
+    CSV.foreach(MARKET_FILE) do |row|
+      markets << FarMar::Market.new(id: row[0].to_i, name: row[1], address: row[2],
+      city: row[3], county: row[4], state: row[5], zip: row[6])
+    end
+    return markets
+
+  end
+
+  def self.find(id)
+    markets = self.all
+      found_id = nil
+      markets.each do |market|
+        if market.id == id
+          found_id = market
+        end
+      end
+      if found_id == nil
+        return "ID not found!"
+      else
+        return found_id
+      end
+
+  end
+
+
+
+
+end
--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,7 +1,19 @@
 require_relative './spec_helper'
 
-describe "FarMar::Market" do
+describe FarMar::Market do
   it "is an object that is an instance of a Class" do
-  FarMar::Market.must_be_instance_of Class
-end
+    FarMar::Market.must_be_instance_of Class
+  end
+
+  describe "FarMar::Market#self.all" do
+    it "should return an incredibly large array" do
+      FarMar::Market.all.must_be_instance_of Array
+    end
+  end
+
+  describe "FarMar::Market#self.find" do
+    it "should return 'People's Co-op Farmers Market' as market name when self.find called by id 1" do
+      FarMar::Market.find(1).name.must_equal "People's Co-op Farmers Market"
+    end
+  end
 end
