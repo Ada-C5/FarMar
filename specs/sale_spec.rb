5ce652cdb441f233c6ce6742f0b62784a32ce0c7
Rowan Cota <cota.rg@gmail.com>
2016-03-14 15:48:16 -0700

    added tests for self#all and self#find.

---
+++ specs/sale_spec.rb
@@ -0,0 +1,30 @@
+require_relative './spec_helper'
+
+describe FarMar::Sale do 
+
+	it "exists" do
+		FarMar::Sale.wont_be_nil
+	end
+
+	it "has an 'all' method that returns an array of sales" do
+		sales = FarMar::Sale.all('./support/sales.csv')
+		sales.wont_be_empty
+	end
+
+	it "populates each object with data from the CSV when #all is called" do
+		sales = FarMar::Sale.all('./support/sales.csv')
+		sale_to_test = rand(1..(sales.length - 1))
+
+		sales[sale_to_test].id.wont_be_empty
+		sales[sale_to_test].amount.wont_be_empty
+	end
+
+	it "has a find method" do
+		FarMar::Sale.find('./support/sales.csv').wont_be_nil
+	end
+
+	it "returns the correct sale when self#find is called" do
+		FarMar::Sale.find(50).amount.must_equal("5420")
+	end
+
+end
