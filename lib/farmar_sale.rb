5c8e4500ac4192d07bf05722a068415e81de6b94
Sarah Nahmias <nahmisa@gmail.com>
2016-03-14 14:42:35 -0700

    Added self.all and self.find to Sale class

--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -1,5 +1,35 @@
 module FarMar
   class Sale
-    "I exist!"
+
+    attr_reader :id
+
+    def initialize(vendor_information)
+      @id                  = vendor_information[:id]
+      @amount              = vendor_information[:amount] #comes from CSV in cents
+      @purchase_time       = vendor_information[:purchase_time]
+      @vendor_id           = vendor_information[:vendor_id]
+      @product_id          = vendor_information[:product_id]
+
+    end
+
+    def self.find(data_file = './support/sales.csv', id) # returns an instance of Sale where the value of the id field in the CSV matches the passed parameter. Will this be different?  There can be multiple with same ID potentially?
+      sales = self.all(data_file)
+      sales.each do |sale|
+        return sale if sale.id == id
+      end
+    end
+
+    def self.all(data_file = './support/sales.csv') #returns a collection of FarMar::"DataClass" instances, representing all of the Markets described in the CSV.
+
+      data_class_instances = [] #start as an empty array. We will fill with instances from our data file.
+
+      data_class_data = CSV.read(data_file)
+      data_class_data.each do |row|
+        class_instance = self.new( id: row[0].to_i, amount: row[1].to_f, purchase_time: DateTime.parse(row[2]), vendor_id: row[3].to_i, product_id: row[4].to_i ) #hash used is different based on type of DataClass
+        data_class_instances << class_instance #put it into our collection of instances!
+      end
+      return data_class_instances
+    end
+
   end
 end
--- specs/sale_spec.rb
+++ specs/sale_spec.rb
@@ -4,4 +4,19 @@ describe FarMar::Sale do
   it "is an object we have access to" do
     FarMar::Sale.wont_be_nil
   end
+
+  describe "FarMar::Sale#all" do
+    it "returns an array of instances" do
+    FarMar::Sale.all.must_be_kind_of(Array)
+    end
+  end
+
+  describe "FarMar::Sale#find" do
+    it "can find a product instance based on ID" do
+    #we have Sale IDs 1-12001, and will test at random!
+    id = (1..12001).to_a.sample
+    FarMar::Sale.find(id).must_be_instance_of(FarMar::Sale)
+    end
+  end
+
 end
