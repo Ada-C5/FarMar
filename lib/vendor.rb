e5bd61bd185f1ab5d2aa3ca477604fb4263c4462
Leah <leahnpetersen@gmail.com>
2016-03-14 14:51:33 -0700

    added self.all and self.find(id) for vendor class, working specs

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,4 +1,6 @@
 class FarMar::Vendor
+  attr_accessor :name, :ven_id
+  FILE = './support/vendors.csv'
 
   def initialize(id, name, num_of_employees, mar_id)
     @ven_id = id
@@ -8,8 +10,23 @@ class FarMar::Vendor
   end
 
   def self.all
+    vendors = []
+    CSV.foreach(FILE) do |line|
+      id = line[0].to_i
+      name = line[1]
+      num_of_employees = line[2].to_i
+      mar_id = line[3].to_i
+      vendors << self.new(id, name, num_of_employees, mar_id)
+    end
+    return vendors
   end
 
   def self.find(id)
+    vendors = self.all 
+    vendors.each do |vendor|
+      if vendor.ven_id == id
+        return vendor
+      end
+    end
   end  
 end
---
+++ specs/vendor_spec.rb
@@ -0,0 +1,22 @@
+require_relative './spec_helper'
+require_relative '../farmar'
+
+describe FarMar::Vendor do
+  # make sure there is a sale class
+  it "Is there a class? Anything?" do
+    FarMar::Vendor.wont_be_nil
+  end
+
+  # make sure self.all works
+  it "should return an array of vendors" do
+    vendors = FarMar::Vendor.all
+    vendors.class.must_equal Array
+    vendors[4].name.must_equal "Reynolds, Schmitt and Klocko"
+  end
+
+  # test self.find(id)
+  it "should return the right account with id" do
+    FarMar::Vendor.find(44).name.must_equal "Kassulke, Leannon and Bins"
+    FarMar::Vendor.find(28).ven_id.must_equal 28
+  end
+end
