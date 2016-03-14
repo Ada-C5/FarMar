cd53ee75f749530a23ca6f4ce62f045eba83b5f3
Sarah <sarahkane682@yahoo.com>
2016-03-14 16:18:38 -0700

    passing tests for sale class self.find and self.all)

--- lib/sale.rb
+++ lib/sale.rb
@@ -2,4 +2,30 @@
 require 'CSV'
 class FarMar::Sale
   include FarMar
+  attr_reader :id, :amount, :purchase_time, :vendor_id, :product_id
+  def initialize(id=nil,amount=nil,purchase_time=nil,vendor_id=nil,product_id=nil)
+    @id = id.to_s
+    @amount = amount
+    @purchase_time = purchase_time
+    @vendor_id = vendor_id
+    @product_id = product_id
+  end
+
+
+  def self.all
+      sales = []
+      CSV.read("./support/sales.csv").each do |line|
+      sales << self.new(line[0],line[1],line[2],line[3],line[4])
+    end
+    return sales #just as a reminder out side of loop
+  end
+
+  def self.find(id)
+    CSV.read("./support/sales.csv").each do |line|
+        if line[0] == id.to_s
+          return self.new(line[0],line[1],line[2],line[3],line[4])
+        end
+      end
+  end
+
 end
--- spec/sale_spec.rb
+++ spec/sale_spec.rb
@@ -1,8 +1,35 @@
 require_relative './spec_helper'
 
 describe FarMar::Sale do
+  before do
+    @sale = FarMar::Sale.new
+  end
 
   it "is an object we have access to" do
-    FarMar::Sale.wont_be_nil
+    @sale.wont_be_nil
   end
+end
+
+describe FarMar::Sale do
+  before do
+  @sale = FarMar::Sale
+  end
+  it "will return an array of sale instances" do
+    @sale.all.class.must_equal Array
+  end
+
+  it "will return a sale with id 5" do
+    @sale.find(5).must_be_instance_of FarMar::Sale
+  end
+
+  it "will return amount of transaction for the sale class" do
+  @sale.find("5").amount.must_equal "4440"
+  end
+
+  it "will return amount of transaction for the sale class" do
+  @sale.find("5").vendor_id.must_equal "1"
+  end
+
+
+
 end
