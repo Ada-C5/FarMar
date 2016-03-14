9aaa5c1bc1931d13867be01d8d36b86c869960f4
Jessica <jessica.weeber@gmail.com>
2016-03-14 12:31:08 -0700

    added first test for FarMar::Market to check that it is an instance of Class

--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,2 +1,8 @@
 require_relative './spec_helper'
 require_relative '../far_mar'
+
+describe "FarMar::Market" do
+  it "is an object that is an instance of a Class" do
+  FarMar::Market.must_be_instance_of Class
+end
+end
