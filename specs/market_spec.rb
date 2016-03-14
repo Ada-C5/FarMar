b9c967efd56cc2de4942fb9037de6f796b345aa3
Rowan Cota <cota.rg@gmail.com>
2016-03-14 14:07:30 -0700

    tests that the all method actually creates objects and pushes them to array

--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,10 +1,18 @@
 require_relative './spec_helper'
-require_relative '../far_mar'
 
 describe FarMar::Market do 
 
 	it "exists" do
 		FarMar::Market.wont_be_nil
+	end
+
+	it "has an 'all' method that returns an array of markets" do
+		markets = FarMar::Market.all('./support/markets.csv')
+		markets.wont_be_empty
+	end
+
+	it "" do
+				
 	end
 
 end
