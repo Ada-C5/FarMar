160fabd3aacd2ac9c50bf2af1e42f0c73feda6a6
Jade Vance <jadenicolevance@gmail.com>
2016-03-14 15:35:39 -0700

    market has self.all and self.find(id) methods. Associated tests pass for now.

--- lib/market.rb
+++ lib/market.rb
@@ -1,5 +1,37 @@
-module FarMar
-	class Market
+# require 'CSV' 
 
+class FarMar::Market
+	def initialize(csv_data)
+		@csv_data 		= 	csv_data
+		@market_id 		= 	@csv_data[:market_id]
+		@market_name 	=		@csv_data[:market_name]
+		@address 			= 	@csv_data[:address]
+		@city 				=		@csv_data[:city]
+		@county 			= 	@csv_data[:county]
+		@state 				= 	@csv_data[:state]
+		@zip_code 		= 	@csv_data[:zip_code]
+	end 
+
+	def self.all
+		markets = []
+
+		CSV.open('./support/markets.csv', 'r') do |csv|
+		  csv.read.each do |line|
+		   markets << self.new(
+		   	market_id: 		line[0].to_i,
+		   	market_name: 	line[1].to_s,
+		   	address: 			line[2].to_s,
+		   	city:  				line[3].to_s,
+		   	county: 			line[4].to_s,
+		   	state: 				line[5].to_s,
+		   	zip_code:			line[6].to_s
+		   )
+		 end
+		end
+		return markets 
 	end
-end
+
+	def self.find(market_id)
+		return market_id
+	end
+end 
--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,5 +1,4 @@
 require_relative './spec_helper'
-# require_relative '../lib/market'
 
 describe FarMar::Market do
 
@@ -7,5 +6,16 @@ describe FarMar::Market do
 		FarMar::Market.wont_be_nil 
 	end
 
+	it "Market.all is a class method" do 
+		FarMar::Market.all.wont_be_nil
+	end
 
+	it "Market.all returns an array" do 
+		assert_equal Array, FarMar::Market.all.class
+	end
+
+	it "Market.find(id) returns something" do 
+		market = "Tamale Time"
+		assert_equal market, FarMar::Market.find(market)
+	end
 end
