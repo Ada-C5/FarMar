c8b32f1c586b76a1f001f2264b5111e8c9827e8d
Jessica <jessica.weeber@gmail.com>
2016-03-14 14:27:09 -0700

    added self.find and self.all method to vendors class. added tests for these methods, all passing.

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,38 @@
 class FarMar::Vendor
+  attr_reader :id, :name, :num_employees, :market_id
+
+  VENDOR_FILE = "support/vendors.csv"
+
+  def initialize(vendor_hash)
+    @id = vendor_hash[:id]
+    @name = vendor_hash[:name]
+    @num_employees = vendor_hash[:num_employees]
+    @market_id = vendor_hash[:market_id]
+  end
+
+  def self.all
+    vendors = []
+    CSV.foreach(VENDOR_FILE) do |row|
+      vendors << FarMar::Vendor.new(id: row[0].to_i, name: row[1], num_employees: row[2].to_i,
+        market_id: row[3].to_i)
+    end
+    return vendors
+
+  end
+
+  def self.find(id)
+    vendors = self.all
+    found_id = nil
+    vendors.each do |vendor|
+      if vendor.id == id
+        found_id = vendor
+      end
+    end
+    if found_id == nil
+      return "ID not found!"
+    else
+      return found_id
+    end
+  end
 
 end
--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -1,1 +1,24 @@
 require_relative './spec_helper'
+
+describe FarMar::Vendor do
+  it "is an object that is an instance of a Class" do
+    FarMar::Vendor.must_be_instance_of Class
+  end
+
+  describe "FarMar::Vendor#self.all" do
+    it "should return an incredibly large array" do
+      FarMar::Vendor.all.must_be_instance_of Array
+    end
+  end
+
+  describe "FarMar::Vendor#self.find" do
+    it "should return 'Jakubowski-Robel' when id 32 passed in" do
+      FarMar::Vendor.find(32).name.must_equal "Jakubowski-Robel"
+    end
+
+    it "should return 'ID not found!' when self.find called by id 3000" do
+      FarMar::Vendor.find(3000).must_equal "ID not found!"
+    end
+
+  end
+end
