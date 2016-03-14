c35d7c2f9e83c40206c3e82af78d5c647f85e765
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-14 16:23:05 -0700

    added initialize method to Vendor class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,10 @@
 class FarMar::Vendor
 
+  def initialize(info_hash)
+    @id = info_hash[:id].to_i
+    @name = info_hash[:name]
+    @number_employees = info_hash[:number_employees].to_i
+    @market_id = info_hash[:market_id].to_i
+  end
+
 end
