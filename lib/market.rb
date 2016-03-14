742dde19b8b476ff8d2135ed184e28560721d5a4
justine <justine.winnie@gmail.com>
2016-03-14 13:07:40 -0700

    Boilerplate & baseline. FarMar module, Market, Product, Sale & Vendor classes created. Passing tests.

---
+++ lib/market.rb
@@ -0,0 +1,30 @@
+# # Pulled tracing the pathway and putting the csv data into an array, into its own method.
+# def read_csv(file)
+#   require 'csv'
+#   allaccountscsv = CSV.read(file, 'r')
+#   return read_csv
+# end
+#
+# # Be able to pull data from a specific line in the csv to an existing account.
+# def acct_from_csv(csv_index)
+#   read_csv("./support/accounts.csv")
+#   @id = allaccountscsv[csv_index][0]
+#   @balance = allaccountscsv[csv_index][1]
+#   @open_date = allaccountscsv[csv_index][2]
+# end
+
+class FarMar::Market
+  # def read_csv(file)
+  #   allmarkets = CSV.read(file, 'r')
+  #   return read_csv
+  # end
+  #
+  # def initialize(allmarkets)
+  #   @market_id
+  #   @address
+  #   @city
+  #   @state
+  #   @zip
+  # end
+
+end
