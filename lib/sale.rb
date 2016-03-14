d1ddfd86af3cad019a0c6ec9ee58fb9be69ebad0
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-14 16:49:40 -0700

    added initialize method to the Sale class

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,3 +1,11 @@
 class FarMar::Sale
 
+    def initialize(info_hash)
+      @id = info_hash[:id].to_i
+      @amount = info_hash[:amount].to_i
+      @purchase_time = info_hash[:purchase_time]
+      @vendor_id = info_hash[:vendor_id].to_i
+      @product_id = info_hash[:product_id].to_i
+    end
+
 end
