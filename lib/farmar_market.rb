d00f11678116f0778fccc57f0ff1125386a86ee2
Rowan Cota <cota.rg@gmail.com>
2016-03-14 13:01:59 -0700

    added a method that knows about the CSV to meet baseline.

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -1,3 +1,19 @@
 class FarMar::Market
 
+	def initialize(id)
+		@id = id
+	end
+
+	def self.all(path_to_csv)
+    id = nil
+
+    market_list = []
+    
+    # this iterates through the CSV and assigns values to variables to be used in the account initialization.
+    CSV.foreach(path_to_csv) do |row|
+        id = row[0]
+        market_list << self.new(id)
+    end
+    market_list
+	end
 end
