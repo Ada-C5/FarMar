51ff264b232c6f736cc5187bb67d9afdef627045
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-15 10:19:42 -0700

    able to call self.all and self.find on FarMar::Product class. added tests to spec file

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,18 @@
 class FarMar::Product
+  attr_reader :id, :name
+
+  extend FarMar::FarMarMethods
 
   def initialize(info_hash)
     @id = info_hash[:id].to_i
     @name = info_hash[:name]
     @vendor_id = info_hash[:vendor_id].to_i
+  end
+
+  def self.all(file = './support/products.csv')
+    product_keys = [:id, :name, :vendor_id]
+
+    make_all(file, product_keys)
   end
 
 end
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -4,4 +4,18 @@ describe FarMar::Product do
   it "is an object that I have access to" do
     FarMar::Product.wont_be_nil
   end
+
+  describe "FarMar::Product class methods" do
+    let(:product_ten) { FarMar::Product.find(10) }
+
+    it "should return an array when calling the self.all method" do
+      FarMar::Product.all.must_be_instance_of Array
+    end
+
+    it "should return a specific instance of FarMar::Product using the self.find method" do
+      product_ten.id.must_equal 10
+      product_ten.name.must_equal "Black Apples"
+      product_ten.must_be_instance_of FarMar::Product
+    end
+  end
 end
