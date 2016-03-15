bcc236f574c4fa85674728c9c44ca056ef8c8568
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-15 10:23:33 -0700

    added comments to the FarMarMethods module

--- lib/far_mar_methods.rb
+++ lib/far_mar_methods.rb
@@ -7,21 +7,26 @@ module FarMar::FarMarMethods
     CSV.open(file, 'r').each do |line|
       instance_hash = {}
 
+      # assigns a value to each key in the hash
       key_array.each_with_index do |key, index|
         instance_hash[key] = line[index]
       end
 
+      # creates a new instance using the hash info
       instance_array << self.new(instance_hash)
     end
 
+    # returns an array full of instances
     return instance_array
   end
 
   def find(id)
+    # iterates through each instance in the collection
     self.all.each do |instance|
       return instance if id == instance.id
     end
 
+    # if an instance matching the given id isn't found, return nil
     nil
   end
   
