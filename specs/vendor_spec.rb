79445b02da899e64dc69666a375c9c904e7385b1
Rowan Cota <cota.rg@gmail.com>
2016-03-15 15:08:46 -0700

    stuff changed.

--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -4,22 +4,22 @@ describe FarMar::Vendor do
 
 	let(:vendor_test_one) { vendor_test_one = FarMar::Vendor.new({id: "1", name: "Feil-Farrell", num_of_employees: "8", market_id: "1"})
  }
+	let(:vendor_test_two) { vendor_test_two = FarMar::Vendor.new({id: "90180", market_id: "10000"})}
+	let(:all_vendors) { all_vendors = FarMar::Vendor.all('./support/vendors.csv')}
 
 	it "exists" do
 		FarMar::Vendor.wont_be_nil
 	end
 
 	it "has an 'all' method that returns an array of vendors" do
-		vendors = FarMar::Vendor.all('./support/vendors.csv')
-		vendors.wont_be_empty
+		all_vendors.wont_be_empty
 	end
 
 	it "populates each object with data from the CSV when #all is called" do
-		vendors = FarMar::Vendor.all('./support/vendors.csv')
-		vendor_to_test = rand(1..(vendors.length - 1))
+		vendor_to_test = rand(1..(all_vendors.length - 1))
 
-		vendors[vendor_to_test].id.wont_be_empty
-		vendors[vendor_to_test].name.wont_be_empty
+		all_vendors[vendor_to_test].id.wont_be_empty
+		all_vendors[vendor_to_test].name.wont_be_empty
 	end
 
 	it "returns the correct sale when self#find is called" do
@@ -34,11 +34,7 @@ describe FarMar::Vendor do
 	end
 
 	it "has a market method that will return nil if no market is found" do
-		# using numbers I know don't currently exist to test the case of not finding a market.
-		vendor_to_test = FarMar::Vendor.new({id: "90180", market_id: "10000"})
-		market = vendor_to_test.market
-
-		market.must_be_nil
+		vendor_test_two.market.must_be_nil
 	end
 
 	it "returns a collection of FarMar::Product instances associated by FarMar::Product vendor_id field" do
@@ -48,14 +44,12 @@ describe FarMar::Vendor do
 	end
 
 	it "returns an empty array when no products are found" do
-		vendor_to_test = FarMar::Vendor.new({id: "90180", market_id: "10000"})
-		vendors_product = vendor_to_test.products
-
-		vendors_product.must_be_empty
-		vendors_product.must_be_instance_of(Array)
+		vendor_test_two.products.must_be_empty
+		vendor_test_two.products.must_be_instance_of(Array)
 	end
 
 	it "has a #sales method" do
+		
 
 	end
 
