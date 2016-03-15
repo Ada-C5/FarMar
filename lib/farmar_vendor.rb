a0a8ab528d87d7c642f4c9999c588184eea6fef1
Valerie <val.conklin@gmail.com>
2016-03-14 17:03:07 -0700

    Started self.all method and corresponding test for farmar_vendor.rb

--- lib/farmar_vendor.rb
+++ lib/farmar_vendor.rb
@@ -7,6 +7,25 @@ class FarMar::Vendor
     @id = vendor_hash[:id]
     @name = vendor_hash[:name]
     @number_of_employees = vendor_hash[:number_of_employees]
-    @market_id = FarMar::Market.id
+    @market_id = FarMar::Market.id # this id ain't working...
+  end
+
+  def self.all
+    # CSV.read reads the contents of the file into an array of arrays
+    all_vendors = CSV.read("./support/vendors.csv")
+    # returns a collection of instances, representing all of the objects described in the CSV
+    all_vendors.collect do |vendor|
+
+      single_vendor_hash = {
+        id: vendor[0],
+        name: vendor[1],
+        number_of_employees: vendor[2]
+      }
+      FarMar::Vendor.new(single_vendor_hash)
+    end
+  end
+
+  def self.find(given_id)
+
   end
 end
--- specs/farmar_vendor_spec.rb
+++ specs/farmar_vendor_spec.rb
@@ -5,3 +5,11 @@ describe FarMar::Vendor do
     FarMar::Vendor.wont_be_nil
   end
 end
+
+describe "FarMar::Vendor#self.all" do
+  it "should return an instance with attributes that are the same as the values
+    in the first line of the CSV file" do
+    # should return the same name
+      FarMar::Vendor.all[0].name.must_equal "Feil-Farrell"
+  end
+end
