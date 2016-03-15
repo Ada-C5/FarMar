1cee1adcb40e2f465566c462b838729d9c704a65
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-15 10:29:49 -0700

    added self.all and self.find (module mixin) methods to the FarMar::Sale class. added specs to test both methods.

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,4 +1,7 @@
 class FarMar::Sale
+  attr_reader :id, :amount
+
+  extend FarMar::FarMarMethods
 
     def initialize(info_hash)
       @id = info_hash[:id].to_i
@@ -8,4 +11,9 @@ class FarMar::Sale
       @product_id = info_hash[:product_id].to_i
     end
 
+    def self.all(file = './support/sales.csv')
+      sale_keys = [:id, :amount, :purchase_time, :vendor_id, :product_id]
+
+      make_all(file, sale_keys)
+    end
 end
--- specs/sale_spec.rb
+++ specs/sale_spec.rb
@@ -4,4 +4,18 @@ describe FarMar::Sale do
   it "is an object that I have access to" do
     FarMar::Sale.wont_be_nil
   end
+
+  describe "FarMar::Sale class methods" do
+    let(:sale_ten) { FarMar::Sale.find(10) }
+
+    it "should return an instance of array using self.all method" do
+      FarMar::Sale.all.must_be_instance_of Array
+    end
+
+    it "should return a specific instance of FarMar::Sale using the self.find method" do
+      sale_ten.must_be_instance_of FarMar::Sale
+      sale_ten.id.must_equal 10
+      sale_ten.amount.must_equal 5160
+    end
+  end
 end
