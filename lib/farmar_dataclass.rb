5bd5fb42372fbbca2a7951c0e74f92f7800f2e61
Sarah Nahmias <nahmisa@gmail.com>
2016-03-15 12:50:27 -0700

    Made self.find a little more reusable so I can use it for Vendor self.by_markets method as well

--- lib/farmar_dataclass.rb
+++ lib/farmar_dataclass.rb
@@ -4,42 +4,47 @@ module FarMar
     def initialize
     end
 
-    def markets(data_file, data_class, id_to_match, method_name) #from vendor
+    def markets(data_file, data_class, id_to_match, method_name)
       match_by(data_file, data_class, id_to_match, method_name)
     end
 
-    #match_by_vendor_id
-    def products(data_file, data_class, id_to_match, method_name) #from vendor
+    def products(data_file, data_class, id_to_match, method_name)
       match_by(data_file, data_class, id_to_match, method_name)
     end
 
-    def sales(data_file, data_class, id_to_match, method_name) #from vendor
+    def sales(data_file, data_class, id_to_match, method_name)
       match_by(data_file, data_class, id_to_match, method_name)
     end
 
-    def vendors(data_file, data_class, id_to_match, method_name) #from market
+    def vendors(data_file, data_class, id_to_match, method_name)
       match_by(data_file, data_class, id_to_match, method_name)
     end
 
     def match_by(data_file, data_class, id_to_match, method_name)
+    # returns an instance where the value of the id field for the instance of data_class (Market, Product, Sale, or Vendor) matches the passed id_to_match parameter.  The method_name returns the ID are trying to match, because it can be market_id, vendor_id, product_id, etc.
+
       all = data_class.all(data_file)
       matched = all.find_all do |instance|
-        id_to_match == instance.send(method_name) #need to do this with send(string) and not just .method_name because otherwise it thinks there is a method called .method name :(
+        id_to_match == instance.send(method_name) # need to do this with send(string) and not just .method_name because otherwise it thinks there is a method called .method name :(
       end
-      matched
+
+      return matched
     end
 
-    def self.find(data_file, id) # returns an instance where the value of the id field in the CSV matches the passed id parameter.
+    def self.find(data_file, method_name = "id", id)
+    # returns an instance where the value of the id field in the CSV matches the passed id parameter.
+    # Uses method_name in the same ways as match_by so we can reuse this code more
+
       all = self.all(data_file)
       all.each do |instance|
-        return instance if instance.id == id
+        return instance if instance.send(method_name) == id
       end
     end
 
-    def self.all(data_file) #returns a collection of FarMar::"DataClass" instances, representing all of the Markets described in the CSV.
+    def self.all(data_file)
+    #returns a collection of FarMar::"DataClass" instances, representing all of the Markets described in the CSV.
 
       data = []
-
       CSV.foreach(data_file) do |row|
         data << row
       end
