ae4f1bca294f8305fd815557a0e607769ef30be3
Sarah Nahmias <nahmisa@gmail.com>
2016-03-15 14:16:02 -0700

    Refactored tests using let for random numbers and new (single) instances in Vendor & market specs

--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,30 +1,29 @@
 require_relative './spec_helper'
 
 describe FarMar::Market do
+  #For all Market Tests, IDs 1-500, and will test at random.
+  let(:random_market_id) { ("1".."500").to_a.sample }
+  let(:market_test) { FarMar::Market.new( [random_market_id, nil, nil, nil, nil, nil, nil] ) }
+
   it "is an object we have access to" do
     FarMar::Market.wont_be_nil
   end
 
-  describe "FarMar::Markets.all" do #how do we show this is a class method?
+  describe "self.all" do #how do we show this is a class method?
     it "returns an array of instances" do
     FarMar::Market.all.must_be_kind_of(Array)
     end
   end
 
-  describe "FarMar::Markets.find" do
+  describe "self.find" do
     it "can find a market instance based on ID" do
-    #we have Market IDs 1-500, and will test at random!
-    id = ("1".."500").to_a.sample
-    FarMar::Market.find(id).must_be_instance_of(FarMar::Market)
+    FarMar::Market.find(random_market_id).must_be_instance_of(FarMar::Market)
     end
   end
 
   describe "FarMar::Markets#vendors" do
     it "returns a collection of FarMar::Vendor instances that are associated with the market by the market_id field." do
-    #we have Market IDs 1-500, and will test at random!
-    id = ("1".."500").to_a.sample
-    test_market = FarMar::Market.new( [id, nil, nil, nil, nil, nil, nil] )
-    vendors_collection = test_market.vendors
+    vendors_collection = market_test.vendors
     vendors_collection.must_be_kind_of(Array)
 
     #any item in the Array will be an instance of FarMar::Vendor
@@ -32,9 +31,6 @@ describe FarMar::Market do
     random_vendor = (0...number_of_vendors).to_a.sample
     vendors_collection[random_vendor].must_be_instance_of(FarMar::Vendor)
     end
-
-
   end
-
 
 end
--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -1,6 +1,15 @@
 require_relative './spec_helper'
 
 describe FarMar::Vendor do
+  #we have Vendor IDs 1-2690, and will test at random
+  let(:random_vendor_id) { ("1".."2690").to_a.sample }
+  #Market IDs are 1-500, and will test at random.
+  let(:random_market_id) { ("1".."500").to_a.sample }
+
+  let(:vendor_by_vendor_id_test) { FarMar::Vendor.new( [random_vendor_id, nil, nil, nil] ) }
+  let(:vendor_by_market_id_test) { FarMar::Vendor.new( [nil, nil, nil, random_market_id] ) }
+
+
   it "is an object we have access to" do
     FarMar::Vendor.wont_be_nil
   end
@@ -13,20 +22,16 @@ describe FarMar::Vendor do
 
   describe "FarMar::Vendor.find" do
     it "can find a product instance based on ID" do
-    #we have Vendor IDs 1-2690, and will test at random!
-    id = ("1".."2690").to_a.sample
-    FarMar::Vendor.find(id).must_be_instance_of(FarMar::Vendor)
+    FarMar::Vendor.find(random_vendor_id).must_be_instance_of(FarMar::Vendor)
     end
   end
 
   describe "FarMar::Vendor.by_market" do
     it "returns *all* of the vendors with the given market_id" do
-    # we have Market IDs 1-500, and will test at random!
-    market_id = ("1".."500").to_a.sample
-    # will return an array of Vendor Instances
-    vendors_collection = FarMar::Vendor.by_market(market_id)
+    vendors_collection = FarMar::Vendor.by_market(random_market_id)
     vendors_collection.must_be_kind_of(Array)
 
+    # any item in the Array will be an instance of FarMar::Vendor
     number_of_vendors = vendors_collection.length
     random_vendor = (0...number_of_vendors).to_a.sample
     vendors_collection[random_vendor].must_be_instance_of(FarMar::Vendor)
@@ -35,10 +40,7 @@ describe FarMar::Vendor do
 
   describe "FarMar::Vendor#markets" do
     it "returns a collection of FarMar::Market instances that are associated with the vendor by the market_id field." do
-    # we have Market IDs 1-500, and will test at random!
-    market_id = ("1".."500").to_a.sample
-    test_vendor = FarMar::Vendor.new( [nil, nil, nil, market_id] )
-    markets_collection = test_vendor.markets
+    markets_collection = vendor_by_market_id_test.markets
     markets_collection.must_be_kind_of(Array)
 
     # any item in the Array will be an instance of FarMar::Market
@@ -50,10 +52,7 @@ describe FarMar::Vendor do
 
   describe "FarMar::Vendor#products" do
     it "returns a collection of FarMar::Product instances that are associated by the FarMar::Product vendor_id field." do
-    # we have Vendor IDs 1-2690, and will test at random!
-    id = ("1".."2690").to_a.sample
-    test_vendor = FarMar::Vendor.new( [id, nil, nil, nil] )
-    products_collection = test_vendor.products
+    products_collection = vendor_by_vendor_id_test.products
     products_collection.must_be_kind_of(Array)
 
     # any item in the Array will be an instance of FarMar::Product
@@ -65,13 +64,10 @@ describe FarMar::Vendor do
 
   describe "FarMar::Vendor#sales" do
     it "returns a collection of FarMar::Sale instances that are associated by the vendor_id field." do
-    # we have Vendor IDs 1-2690, and will test at random!
-    id = ("1".."2690").to_a.sample
-    test_vendor = FarMar::Vendor.new( [id, nil, nil, nil] )
-    sales_collection = test_vendor.sales
+    sales_collection = vendor_by_vendor_id_test.sales
     sales_collection.must_be_kind_of(Array)
 
-    # any item in the Array will be an instance of FarMar::Product
+    # any item in the Array will be an instance of FarMar::Sale
     number_of_sales = sales_collection.length
     random_sale = (0...number_of_sales).to_a.sample
     sales_collection[random_sale].must_be_instance_of(FarMar::Sale)
@@ -80,10 +76,7 @@ describe FarMar::Vendor do
 
   describe "FarMar::Vendor#revenue" do #this one won't work yet!
     it "returns the the sum of all of the vendor's sales (in cents)" do
-    # we have Vendor IDs 1-2690, and will test at random!
-    id = ("1".."2690").to_a.sample
-    test_vendor = FarMar::Vendor.new( [id, nil, nil, nil] )
-    test_vendor.revenue.must_be_kind_of(Float) #we want the number back in cents!
+    vendor_by_vendor_id_test.revenue.must_be_kind_of(Float) #we want the number back in cents!
 
     end
   end
