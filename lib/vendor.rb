4f52611e92c3c079cc5adbc2e98a6320e5b04c0f
Leah <leahnpetersen@gmail.com>
2016-03-16 10:32:33 -0700

    refactored vendor class

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -13,21 +13,14 @@ class FarMar::Vendor
   def self.all
     vendors = []
     CSV.foreach(FILE) do |line|
-      id = line[0].to_i
-      name = line[1]
-      num_of_employees = line[2].to_i
-      mar_id = line[3].to_i
-      vendors << self.new(id, name, num_of_employees, mar_id)
+      vendors << self.new(line[0].to_i, line[1], line[2].to_i, line[3].to_i)
     end
     return vendors
   end
 
   # return one vendor with a specific id
   def self.find(id)
-    vendors = self.all 
-    vendors.each do |vendor|
-      return vendor if vendor.ven_id == id
-    end
+    self.all.find { |vendor| vendor.ven_id == id}
   end  
 
   # find all vendors with given market_id
