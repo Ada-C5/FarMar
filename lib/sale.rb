a3fc141e304ecce1b25f7338cbbb128d0e53aa86
Yordanos <yordanosd@gmail.com>
2016-03-16 11:41:12 -0700

    completed the FarMar::Sale#self.between method using chronic gem to accept user input and cover? to check the sale dates is in the range

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,19 +1,20 @@
 #require_relative '../far_mar'
 require 'date'
+require 'chronic'
 
 class FarMar::Sale
   attr_reader :id, :amount, :purchase_time, :vendor_id, :product_id
-
+  FILE = "./support/sales.csv"
   def initialize(sale_info)
     @id             = sale_info[ :id ]            # ((Fixnum) uniquely identifies the product
     @amount         = sale_info[ :amount ]        # (Fixnum) the amount of the transaction, in cents (i.e., 150 would be $1.50)
-    @purchase_time  = DateTime.parse(sale_info[:purchase_time]) # (Datetime) when the sale was completed
+    @purchase_time  = Time.parse(sale_info[:purchase_time]) # (Datetime) when the sale was completed
     @vendor_id      = sale_info[ :vendor_id ]     # (Fixnum) a reference to which vendor completed the sale
     @product_id     = sale_info[ :product_id ]     # (Fixnum) a reference to which product was sold
   end
 
   def self.all
-    sale_csv_info = CSV.read("./support/sales.csv")
+    sale_csv_info = CSV.read(FILE)
     sale_csv_info.map {|line| self.new(id: line[0].to_i, amount: line[1].to_i, purchase_time: line[2], vendor_id: line[3], product_id: line[4] )}
   end
 
@@ -28,4 +29,18 @@ class FarMar::Sale
     FarMar::Vendor.all.select {|vendor| vendor.id.to_i == vendor_id.to_i}
   end
 
+  def product
+    FarMar::Product.all.select { |product| product.id.to_i == product_id.to_i}
+  end
+
+  def self.between(beginning_time, end_time)
+    start_date = Chronic.parse(beginning_time)
+    end_date   = Chronic.parse(end_time)
+    result = self.all.select {|sale| (start_date..end_date).cover? (sale.purchase_time) }
+    return result
+  end
+
+  # def min_max
+  #   self.all.minmax_by {|date| date.purchase_time}
+  # end
 end
--- specs/sale_spec.rb
+++ specs/sale_spec.rb
@@ -30,4 +30,30 @@ describe FarMar::Sale do
       vendors.uniq.must_equal [FarMar::Vendor]
     end
   end
+
+  # FarMar::Sale. product_id == FarMar::Product.id
+  describe "FarMar::Sales#product" do
+    it "returns FarMar product instances that correspond with a sales instance" do
+      product_list = sale_12.product.map {|product| product.class}
+      product_list.uniq.must_equal [FarMar::Product]
+    end
+  end
+
+  describe "FarMar::Sales#self.between(beginning_time, end_time)" do
+    it "should return a collection of FarMar::Sale instances" do
+      sale_result = sale.between("2013-11-12 06:03:54 -0800", "2013-11-12 01:22:56 -0800")
+      sale_result.wont_be_nil
+    end
+
+    it "return an array with of instances that fit the range" do
+      sale_result = sale.between("2013-11-08 16:36:03 -0800", "2013-11-12 01:22:56 -0800")
+      sale_result.class.must_equal Array
+    end
+
+    it "return a specific instance in that range" do
+      sale_result = sale.between("2013-11-08 16:36:03 -0800", "2013-11-09 01:22:56 -0800")
+      sale_result_select = sale_result.select {|sale| sale.purchase_time == Time.parse("2013-11-08 16:36:03 -0800")}
+      sale_result_select.wont_be_nil
+    end
+  end
 end
