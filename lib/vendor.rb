e359fb6b93afcbebc00aa8984aaaf5ef63fc8189
Lisa Rolczynski <lisa.sano@gmail.com>
2016-03-16 10:45:00 -0700

    added method comments to clarify whether each returns a single instance or an array of instances

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -16,24 +16,29 @@ class FarMar::Vendor
     make_all(file_name, vendor_keys)
   end
 
+  # returns all vendors with the given market id
   def self.by_market(id_of_market)
     self.all.find_all do |vendor|
       vendor.market_id == id_of_market
     end
   end
 
+  # returns the associated market instance
   def market
     FarMar::Market.find(market_id)
   end
 
+  # returns an array of associated products
   def products
     FarMar::Product.by_vendor(id)
   end
 
+  # returns an array of associated sales
   def sales
     FarMar::Sale.get_by("vendor", id)
   end
 
+  # returns the sum of all sales in cents
   def revenue
     sales.reduce(0) { |sum, sale| sum + sale.amount }
   end
