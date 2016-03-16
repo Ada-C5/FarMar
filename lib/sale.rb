64366cb4fc2e09cd6374c701f52058f6759f70a6
Leah <leahnpetersen@gmail.com>
2016-03-16 10:29:59 -0700

    refactored sale class

--- lib/sale.rb
+++ lib/sale.rb
@@ -13,28 +13,19 @@ class FarMar::Sale
   def self.all
     sales = []
     CSV.foreach(FILE) do |line|
-      id = line[0].to_i
-      amount = line[1].to_i
-      purchase_time = Time.parse(line[2])
-      ven_id = line[3].to_i
-      prod_id = line[4].to_i
-      sales << self.new(id, amount, purchase_time, ven_id, prod_id)
+      sales << self.new(line[0].to_i, line[1].to_i, Time.parse(line[2]), line[3].to_i, line[4].to_i)
     end
     return sales
   end
 
   def self.find(id)
-    sales = self.all 
-    sales.each do |sale|
-      return sale if sale.sale_id == id
-    end
+    self.all.find { |sale| sale.sale_id == id }
   end
 
   # method to find sales that happened in a certain range
   def self.between(start_time, end_time)
-    start_time = Time.parse(start_time)
-    end_time = Time.parse(end_time)
-    self.all.find_all { |sale| (start_time..end_time).include?(sale.purchase_time) }
+    # make new time instances with start/end time and use them as a range to search
+    self.all.find_all { |sale| (Time.parse(start_time)..Time.parse(end_time)).include?(sale.purchase_time) }
   end
 
   # return vendor instance related to sale
