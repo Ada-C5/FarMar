92cb4f7a4cd77f9b01247551c3c49a9868abeeab
Anna Wilson <annie.marie.mason@gmail.com>
2016-03-17 14:10:18 -0700

    Added market specs.

--- specs/farmar_market_spec.rb
+++ specs/farmar_market_spec.rb
@@ -1,6 +1,9 @@
 require_relative './spec_helper'
 
 describe FarMar::Market do
+	before do
+		@test_market = FarMar::Market.find(1)[0]
+	end
 
 	it "FarMar::Market Class Exists" do
 		FarMar::Market.wont_be_nil
@@ -32,39 +35,38 @@ describe FarMar::Market do
 		end
 
 		it "returns an instance of FarMar::Market when given a valid id" do
-			FarMar::Market.find(1).must_be_instance_of FarMar::Market
+			@test_market.must_be_instance_of FarMar::Market
 		end
 
-		it "returns nil when given an invalid id" do
-			FarMar::Market.find(9999999).must_be_nil
+		it "returns [] when given an invalid id" do
+			FarMar::Market.find(9999999).must_equal []
 		end
 
 		it "returns the correct instance when given a valid id" do
-			FarMar::Market.find(1).city.must_equal "Portland"
+			@test_market.city.must_equal "Portland"
 		end
 	end
 
-	describe "self.vendors" do
+	describe "vendors method" do
 		it "exists" do
-			FarMar::Market.vendors(1).wont_be_nil
+			@test_market.vendors.wont_be_nil
 		end
 
 		it "returns an array" do
-			FarMar::Market.vendors(1).must_be_instance_of Array
+			@test_market.vendors.must_be_instance_of Array
 		end
 
 		it "returns an array containing FarMar::Vendor instance(s)" do
-			FarMar::Market.vendors(1)[0].must_be_instance_of FarMar::Vendor
+			@test_market.vendors[0].must_be_instance_of FarMar::Vendor
 		end
 
 		it "returns the correct instances of FarMar::Vendor" do
-			test = FarMar::Market.vendors(30)
-			test[0].name.must_equal "Gleichner and Sons"
+			(FarMar::Market.find(30))[0].name.must_equal "Grafton Farmers Market"
 		end
 
 		# it "returns all correct instances of FarMar::Vendor"
 		# tests a vendor id that has multiple matches somehow.
-		
+
 	end
 
 end
