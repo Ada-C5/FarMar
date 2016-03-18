10dbbce54ceb605bd449c315b144228eb18e34b6
Rowan Cota <cota.rg@gmail.com>
2016-03-18 14:17:17 -0700

    better find method written.

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -42,18 +42,9 @@ class FarMar::Market
     market_list
   end
 
-
   def self.find(id)
     markets_to_search = FarMar::Market.all(FarMar::MARKETS_CSV)
-    found_market = nil
-
-    markets_to_search.each do |market|
-      if market.id == id.to_s
-        found_market = market
-      end
-    end
-
-    found_market
+    markets_to_search.find {|market| market.id == id.to_s}
   end
 
   def vendors
