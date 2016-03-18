f4ca1cfaa059a9da7f60bdea57f73409466059e5
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-03-18 10:42:24 -0700

    Put common methods into a shared_methods class to inherit common behavior, altered far_mar to require additional necessary class

--- far_mar.rb
+++ far_mar.rb
@@ -6,6 +6,7 @@ module FarMar;end
 
 
 # all of our data classes that live in the module
+require_relative 'lib/shared_methods'
 require_relative 'lib/farmar_market'
 require_relative 'lib/farmar_vendor'
 require_relative 'lib/farmar_sale'
