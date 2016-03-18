42776afe3303679e83eaf97d4ab9c50e6eb221ab
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-03-18 13:36:05 -0700

    'Fixed' time comparison - only calculates on dates, not specific time. Derp.

--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -1,9 +1,10 @@
 class FarMar::Sale < FarMar::SharedStuff
   attr_reader :sale_id, :amount, :purchase_time, :vendor_id, :product_id
   def initialize(line) #for all, csv length -1?
+
     @sale_id = line[0].to_i
-    @amount = line[1].to_i # see if can => dollars, even with concern over nil *
-    @purchase_time = line[2].to_i
+    @amount = line[1].to_i
+    @purchase_time = line[2].to_i #unix_time, or as close as I could get.
     @vendor_id = line[3].to_i
     @product_id = line[4].to_i
   end
@@ -13,6 +14,10 @@ class FarMar::Sale < FarMar::SharedStuff
   end
 
   def self.make_new_instance(instance)
+    to_unix_time = instance[2].split("-")
+    to_unix_time[2] = to_unix_time[2].split
+    to_unix_time[2] = to_unix_time[2].first
+    instance[2] = Date.new(to_unix_time[0].to_i,to_unix_time[1].to_i,to_unix_time[2].to_i).to_time
     FarMar::Sale.new(instance)
   end
 
@@ -26,8 +31,16 @@ class FarMar::Sale < FarMar::SharedStuff
     return product.values[0] #returns instance
   end
 
-  def self.between(beginning_time = 0, end_time = Time.now.to_i)
-    hash_of_instances = self.all.select {|sid, instance| instance.purchase_time > beginning_time.to_i && instance.purchase_time< end_time.to_i}
-    hash_of_instances.values #returns an array filled with instances
+  def self.between(beginning_time, end_time)
+    end_array = end_time.split("/")
+    end_array.map! {|string| string.to_i}
+    end_time = Date.new(end_array[2], end_array[0], end_array[1]).to_time
+
+    start_array = beginning_time.split("/")
+    start_array.map! {|string| string.to_i}
+    beginning_time = Date.new(start_array[2], start_array[0], start_array[1]).to_time
+
+    hash_of_instances = self.all.select {|sid, instance| instance.purchase_time.to_i >= beginning_time.to_i && instance.purchase_time.to_i <= end_time.to_i}
+    return hash_of_instances.values
   end
 end
